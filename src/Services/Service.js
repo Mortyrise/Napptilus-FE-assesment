dbb0a769f70039caf92d97ac3ea9dfa6c97f1ca9
JoanMarc <joanmarcdomenech@gmail.com>
2022-07-27 10:06:32 +0200

    Architecture implemented, components, pages and Services

---
+++ src/Services/Service.js
@@ -0,0 +1,22 @@
+const URL = 'https://front-test-api.herokuapp.com/';
+
+async function getAllProducts() {
+  try {
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+async function getProductDetailsByID() {
+  try {
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+async function addProductToCart() {
+  try {
+  } catch (err) {
+    throw new Error(err);
+  }
+}
